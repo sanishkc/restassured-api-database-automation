f2a54a68901ef5e89322359483c609ad0c52963a
Sanish Chandran <sanish.k.c@gmail.com>
2026-01-12 13:38:31 +0200

    CICD Changes

--- src/test/java/com/api/utils/ConfigManager.java
+++ src/test/java/com/api/utils/ConfigManager.java
@@ -5,19 +5,6 @@ import java.util.Properties;
 
 public class ConfigManager {
 
-	/*
-	 * private static Properties properties = new Properties();
-	 * 
-	 * static { try { InputStream input = ConfigManager.class .getClassLoader()
-	 * .getResourceAsStream("config.properties"); properties.load(input); } catch
-	 * (Exception e) { throw new
-	 * RuntimeException("Failed to load config.properties"); } }
-	 * 
-	 * public static String get(String key) { return properties.getProperty(key); }
-	 */
-	
-
-
     private static final Properties properties = new Properties();
 
     static {
